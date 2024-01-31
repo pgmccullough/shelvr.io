8f2378a8ea60d3ae833ca94759c059b517f86715
Patrick McCullough <p@mccullo.ug>
2024-01-30 22:04:32 -0500

    sectionalize page

--- src/components/Pallet/Pallet.tsx
+++ src/components/Pallet/Pallet.tsx
@@ -3,21 +3,25 @@ import { type FC } from 'react';
 import { type Board } from '../../types/AppTypes';
 import style from './Pallet.module.scss';
 
-
 export const Pallet: FC<{cutList: Array<Board>}> = ({ cutList }) => {
     return (
-        <section className={style['pallet']}>
-            {cutList.map((board: Board) => 
-                <article className={style['pallet__item']}>
-                    <Model
-                        key={board.id}
-                        width={board.width}
-                        length={board.length}
-                        depth={board.depth}
-                        finish={board.finish}
-                    />
-                </article>
-            )}
-        </section>
+        !cutList.length 
+        ? ""
+        : <div>
+            <h2>My Pallet</h2>
+            <section className={style['pallet']}>
+                {cutList.map((board: Board) => 
+                    <article className={style['pallet__item']}>
+                        <Model
+                            key={board.id}
+                            width={board.width}
+                            length={board.length}
+                            depth={board.depth}
+                            finish={board.finish}
+                        />
+                    </article>
+                )}
+            </section>
+        </div>
     )
 }
