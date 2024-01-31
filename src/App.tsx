0e53b9610a51dcfe608fe559008a3ffd81003328
Patrick McCullough <p@mccullo.ug>
2024-01-30 22:11:04 -0500

    header info

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header cutList={cutList} />
       <Hero setCutList={setCutList} />
       <Pallet cutList={cutList} />
     </>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
 import style from "./Header.module.scss";
+import { Board } from "../../types/AppTypes";
 
-export const Header: FC = () => {
+export const Header: FC<{cutList: Array<Board>}> = ({ cutList }) => {
     return (
-        <header className={style.header}></header>
+        <header className={style.header}>
+            my pallet: {cutList.length}
+        </header>
     )
 }
